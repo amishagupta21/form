48edbf86b79f4b9e509333be9e2f8d94672502f4
amishagupta21 <98382043+amishagupta21@users.noreply.github.com>
2022-02-22 14:40:54 +0530

    Add files via upload

---
+++ Message.tsx
@@ -0,0 +1,204 @@
+/*
+  This example requires Tailwind CSS v2.0+ 
+  
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+import React, { useState } from "react";
+import { MailIcon, PhoneIcon } from '@heroicons/react/outline'
+
+export default function Example() {
+  const [disabled, setDisabled] = useState(true);
+  const [popUp, setPopUp] = useState(false);
+  const [processing, setProcessing] = useState(false);
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+  const handler = (event: any) => {
+
+    if (event.target.name === "fullname") {
+      setFullname(event.target.value)
+    }
+    if (event.target.name === "email") {
+      setEmail(event.target.value)
+    }
+    if (event.target.name === "message") {
+      setMessage(event.target.value)
+      if (event.target.value.length === 0) {
+        setDisabled(true)
+        return
+      }
+      setDisabled(false)
+    }
+  }
+
+  const submit = (event: any) => {
+    event.preventDefault();
+    setProcessing(true)
+    setTimeout(() => {
+      setPopUp(true);
+      setFullname("");
+      setEmail("");
+      setMessage("");
+      setDisabled(true);
+      setProcessing(false);
+    }, 10000)
+  }
+
+  const closePopUp = () => {
+    setPopUp(false);
+  }
+  return (
+    <div className="relative bg-white">
+      <div className="absolute inset-0">
+        <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50" />
+      </div>
+      <div className="relative max-w-7xl mx-auto lg:grid lg:grid-cols-5">
+        <div className="bg-gray-50 py-16 px-4 sm:px-6 lg:col-span-2 lg:px-8 lg:py-24 xl:pr-12">
+          <div className="max-w-lg mx-auto">
+            <h2 className="text-2xl font-extrabold tracking-tight text-gray-900 sm:text-3xl">Get in touch</h2>
+            <p className="mt-3 text-lg leading-6 text-gray-500">
+              Reach us out to get more information about us!
+            </p>
+            <dl className="mt-8 text-base text-gray-500">
+              <div>
+                <dt className="sr-only">Postal address</dt>
+                <dd>
+                  <p>742 Evergreen Terrace</p>
+                  <p>Springfield, OR 12345</p>
+                </dd>
+              </div>
+              <div className="mt-6">
+                <dt className="sr-only">Phone number</dt>
+                <dd className="flex">
+                  <PhoneIcon className="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
+                  <span className="ml-3">+1 (555) 123-4567</span>
+                </dd>
+              </div>
+              <div className="mt-3">
+                <dt className="sr-only">Email</dt>
+                <dd className="flex">
+                  <MailIcon className="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
+                  <span className="ml-3">support@example.com</span>
+                </dd>
+              </div>
+            </dl>
+            
+          </div>
+        </div>
+        <div className="bg-white py-16 px-4 sm:px-6 lg:col-span-3 lg:py-24 lg:px-8 xl:pl-12">
+          <div className="max-w-lg mx-auto lg:max-w-none">
+            <form action="#" method="POST" className="grid grid-cols-1 gap-y-6">
+              <div>
+                <label htmlFor="full-name" className="sr-only">
+                  Full name
+                </label>
+                <input
+                  type="text"
+                  name="fullname"
+                  value={fullname}
+                  onChange={handler}
+                  id="full-name"
+                  autoComplete="name"
+                  className="block w-full shadow-sm border-2 py-3 px-4 placeholder-gray-500 focus:ring-red-500 focus:border-red-600 outline-none border-gray-300 rounded-md"
+                  placeholder="Full name"
+                />
+              </div>
+              <div>
+                <label htmlFor="email" className="sr-only">
+                  Email
+                </label>
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  value={email}
+                  onChange={handler}
+                  autoComplete="email"
+                  className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-red-500 focus:border-red-600 outline-none border-gray-300 border-2 rounded-md"
+                  placeholder="Email"
+                />
+              </div>
+
+              <div>
+                <label htmlFor="message" className="sr-only">
+                  Message
+                </label>
+                <textarea
+                  id="message"
+                  name="message"
+                  rows={4}
+                  value={message}
+                  onChange={handler}
+                  className="block w-full shadow-sm py-3 px-4 placeholder-gray-500 focus:ring-red-500 focus:border-red-600  outline-none border-gray-300 border-2 rounded-md"
+                  placeholder="Message"
+                  defaultValue={''}
+                />
+              </div>
+              <div>
+                <button
+                  style={{ opacity: disabled ? "0.6" : "1" }}
+                  disabled={disabled}
+                  type='submit'
+                  onClick={submit}
+                  className="inline-flex justify-center py-3 px-6 border border-transparent shadow-sm text-base font-medium rounded-md text-white bg-red-600 hover:bg-red-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500"
+                >
+                  {processing ? "Sending" : "Submit"}
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      {/* Popup */}
+
+      {popUp &&
+        <div style={{
+          width: "100%",
+          height: "100vh",
+          
+          backgroundColor: "rgba(0,0,0,0.6)",
+          position: "fixed",
+          top: 10,
+          right: 0,
+          zIndex: 222,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+         
+        }}>
+          <div style={{
+            width: "40%",
+            height: "20%",
+            backgroundColor: "#fff",
+            borderRadius: 10,
+            
+
+          }}>
+           <button onClick={closePopUp} > <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg></button>
+          
+            <h1>Data Submitted Successfully</h1> 
+            <h5>We will get back to you shortly, Thank you!</h5>
+            <button onClick={closePopUp} >Okay</button>
+          </div>
+
+        </div>}
+    </div>
+  )
+}
+
+
+
+
